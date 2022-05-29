717c9c085c52220cd4b4b0e25938cccfbfc0b65c
Linyuan Gong <gonglinyuan@hotmail.com>
2022-05-29 14:16:02 -0400

    Fix import

--- efficent_large_lm_trainer/data/__init__.py
+++ efficent_large_lm_trainer/data/__init__.py
@@ -1,8 +1,8 @@
-from constant_dataset import ConstantDataset
-from pad_shift_dataset import PadShiftDataset
-from t5_dataset import T5Dataset
-from table_lookup_dataset import TableLookupDataset
-from tensor_list_dataset import TensorListDataset
+from .constant_dataset import ConstantDataset
+from .pad_shift_dataset import PadShiftDataset
+from .t5_dataset import T5Dataset
+from .table_lookup_dataset import TableLookupDataset
+from .tensor_list_dataset import TensorListDataset
 
 __all__ = [
     "ConstantDataset",
