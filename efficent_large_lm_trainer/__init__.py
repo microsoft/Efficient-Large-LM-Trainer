72b3e48e9fbf3f11a8112ef5336edb62469011c6
Linyuan Gong <gonglinyuan@hotmail.com>
2022-05-29 20:53:44 -0400

    Try to fix criterions import

--- efficent_large_lm_trainer/__init__.py
+++ efficent_large_lm_trainer/__init__.py
@@ -1,1 +1,14 @@
-import criterions.enc_dec_cross_entropy
+import importlib
+import os
+
+# automatically import any Python files in the current directory
+cur_dir = os.path.join(os.path.dirname(__file__), "criterions")
+for file in os.listdir(cur_dir):
+    path = os.path.join(cur_dir, file)
+    if (
+        not file.startswith("_")
+        and not file.startswith(".")
+        and (file.endswith(".py") or os.path.isdir(path))
+    ):
+        mod_name = file[: file.find(".py")] if file.endswith(".py") else file
+        module = importlib.import_module(__name__ + "." + mod_name)
